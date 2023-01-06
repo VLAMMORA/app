320d83ac7b7ed4ad140f5cad7efae7d92bc35d46
VLAMMORA <you@example.com>
2023-01-05 20:57:03 -0600

    Maquetacion del proyecto NAV, FOOTER, HOME

---
+++ src/Components/Footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+
+class Footer extends Component {
+    
+    state = {  } 
+    render() { 
+        return (
+
+            <footer class="bg-dark text-center text-lg-start mt-5">
+          
+            <div class="text-center p-3" >
+              © 2023 Copyright
+              <a class="text-white" href="https://google.com/"> Marcelo M</a>
+            </div>
+        
+          </footer>
+        );
+    }
+}
+ 
+export default Footer;
